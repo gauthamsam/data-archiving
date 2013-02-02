f94319d0e5966208d8ed2500ab3ce920cbe79bea
Gautham <gautham.n.sam@gmail.com>
2013-02-01 22:22:44 -0800

    Changed InputReceiver to correctly accept the type of operation. Changed some Constants as well.

--- src/client/InputReceiver.java
+++ src/client/InputReceiver.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import api.StorageClient;
 
@@ -47,18 +49,17 @@ public class InputReceiver extends Thread{
 				connection = service.accept();
 				
 				InputStream in = connection.getInputStream();
-	            
-	            DataInputStream dis = new DataInputStream(in);
-	            
-	            int operation = 0;
-	            byte[] hash = new byte[20];
-	            int dataLength = 0;	            
-	            int offset = 0;	            
-	            int bytesToRead = 0;
-	            byte[] buffer = null;
-	            byte[] data = null;
-	            
-	            while(true) {
+				DataInputStream dis = new DataInputStream(in);
+				
+				int operation = 0;
+				byte[] hash = new byte[20];
+				int dataLength = 0;
+				int offset = 0;
+				int bytesToRead = 0;
+				byte[] buffer = null;
+				byte[] data = null;
+				
+				while(true) {
 	            	// re-initialize all the values.
 	            	offset = 0;
 	            	operation = 0;
@@ -77,24 +78,27 @@ public class InputReceiver extends Thread{
 	            		break;
 	            	}
 	            	
+	            	// Only 2 bytes are used for 'operation'.
 	            	for(int i = 0; i < 2; i++) {
 	            		operation = (operation << 8) | buffer[offset++];
 	            	}
+	            	operation <<= 16;
 	            	
 	            	operation = littleToBigEndian(operation);
 	            	
 	            	System.out.println("Operation " + operation);
 	            	
 	            	for(int i = 0; i < 20; i++) {
-	            		hash[i] = buffer[offset++];
+	            		// little endian to big endian
+	            		hash[20 - i - 1] = buffer[offset++];
 	            	}
 	            	
 	            	System.out.println("Hash " + new String(hash));
 	            	
-	            	dataLength = bytesToRead - offset;
-	            	System.out.println("dataLength " + dataLength);
-	            	
-	            	if (operation == 0) { // put operation            		
+	            	if (operation == 0) { // put operation
+		            	dataLength = bytesToRead - offset;
+		            	System.out.println("dataLength " + dataLength);
+		            	
 	            		data = new byte[dataLength];
 	            		for(int i = 0; i < data.length; i++) {
 	            			data[i] = buffer[offset++];
@@ -130,7 +134,12 @@ public class InputReceiver extends Thread{
 	 * @return the converted integer.
 	 */
 	private int littleToBigEndian(int i) {
-	    return ((i & 0xff) << 24) + ((i & 0xff00) <<8 ) + ((i & 0xff0000) >> 8) + ((i >> 24) & 0xff);
+		ByteBuffer bbuf = ByteBuffer.allocate(4);  
+        bbuf.order(ByteOrder.LITTLE_ENDIAN);  
+        bbuf.putInt(i);
+        bbuf.order(ByteOrder.BIG_ENDIAN);  
+        return bbuf.getInt(0);
 	}
+	
 	
 }
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -9,10 +9,10 @@ package utils;
 public class Constants {
 
 	/** The Constant BUCKET_DIR. */
-	public static final String BUCKET_DIR = "/home/sam/ucsb/data-archiving/storage";
+	public static final String BUCKET_DIR = "/home/gautham/data-archiving/storage";
 	
 	/** The Constant DATA_DIR. */
-	public static final String DATA_DIR = "/home/sam/ucsb/data-archiving/storage";
+	public static final String DATA_DIR = "/home/gautham/data-archiving/storage";
 	
 	/** The Constant BUCKET_FILE_EXTENSION. */
 	public static final String BUCKET_FILE_EXTENSION = ".bucket";
@@ -21,16 +21,10 @@ public class Constants {
 	public static final String DATASTORE_FILE_EXTENSION = ".data";
 	
 	/** The Constant BUCKET_HASH_NUM_BYTES. */
-	public static final int BUCKET_HASH_NUM_BITS = 8;
+	public static final int BUCKET_HASH_NUM_BITS = 1;
 
 	/** The size of the buffer. Once the number of tasks in the scheduler queue exceeds this, the bucket becomes ready to be scheduled. */
 	public static final int BUFFER_SIZE = 3;
-	
-	/** The Constant TASK_TYPE_PUT. */
-	public static final int TASK_TYPE_PUT = 1;
-	
-	/** The Constant TASK_TYPE_GET. */
-	public static final int TASK_TYPE_GET = 2;
 	
 	/** The Constant THREADS_PER_PROCESSOR. */
 	public static final int THREADS_PER_PROCESSOR = 4;
